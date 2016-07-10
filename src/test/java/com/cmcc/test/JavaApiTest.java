738d0be3af645d9230da5a21fa5352ed7c26f019
scoldfield <530314297@qq.com>
2016-07-10 21:05:54 +0800

    增加测试类

--- src/test/java/com/cmcc/test/JavaApiTest.java
+++ src/test/java/com/cmcc/test/JavaApiTest.java
@@ -2,6 +2,7 @@ package com.cmcc.test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.Test;
@@ -14,6 +15,13 @@ public class JavaApiTest {
         Date parseDate = new SimpleDateFormat("yyyy-mm-dd").parse(date);
         
         System.out.println("parseDate = " + parseDate);
+    }
+    
+    @Test
+    public void getYearTest() throws ParseException {
+    	Calendar calendar = Calendar.getInstance();
+    	int year = calendar.get(Calendar.YEAR);
+    	System.out.println("year = " + year);
     }
 
 }
